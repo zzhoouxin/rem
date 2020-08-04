6ee89d5c2f51da6594e5666a42bca5e8f2af6750
zhouxin <zxh87516@qq.com>
2020-08-04 19:01:16 +0800

    375

--- rem.js
+++ rem.js
@@ -7,13 +7,13 @@ var calculatSeize = function () {
 	    clientWidth = docEl.clientWidth;
 		clientHeight = docEl.clientHeight;
 	if (!clientWidth) return;
-	var html_font_size = BASE_FONT_SIZE * ((clientWidth*roat) / 750);
+	var html_font_size = BASE_FONT_SIZE * ((clientWidth*roat) / 350);
 	docEl.style.fontSize = html_font_size + 'px';
 	// 如果只是在相应的-0.01~0.01之间的小数值，直接进行return掉
 	if (html_font_size-parseFloat(getComputedStyle(docEl).fontSize)<0.01 && html_font_size-parseFloat(getComputedStyle(docEl).fontSize)>-0.01) {
 		// 计算准确直接跳出
 		return;
-	} 
+	}
 	// 在曲面屏手机存在着rem计算不准的问题
 	else {
 		var again_html_font_size = html_font_size/(parseInt(getComputedStyle(docEl).fontSize)/html_font_size);
